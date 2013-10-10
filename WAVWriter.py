84d42932ac481da4117de00bb891478c321e7829
Dylan Fried <dylan.fried@gmail.com>
2013-10-10 09:05:55 -0700

    simple pysynth wrapper

---
+++ WAVWriter.py
@@ -0,0 +1,15 @@
+import pysynth
+
+class WAVWriter:
+    keys_s = ('a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#')
+    def __init__(self):
+        pass
+    def write(self, melody,title):
+        abc_notation = []
+        for note in melody:
+            abc_notation.append((self.number_to_abc(note),4))
+        pysynth.make_wav(abc_notation,fn=title)
+    def number_to_abc(self,number):
+        octave = (number+9) // 12
+        note = '%s%u' % (self.keys_s[number%12], octave)
+        return note
