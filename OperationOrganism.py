16a420cd2055498ef7e4c230b55be812e645fa93
Dylan Fried <dylan.fried@gmail.com>
2014-03-26 22:22:53 -0500

    more stuff

---
+++ OperationOrganism.py
@@ -0,0 +1,121 @@
+from copy import deepcopy
+import random
+from helpers import shift
+
+class OperationOrganism:
+
+    def __init__(self,genome=None):
+        if genome:
+            self.genome = genome[:]
+        else:
+            self.genome = [(random.randint(1,5),random.randint(-20,20)) for i in range(100)]
+    
+    def mutate(self,gene_range,mutate_max):
+        ''' Return a mutated organism '''
+        c = self.copy()
+        for i in range(len(c.genome)):
+            if random.random() < 0.2:
+                if random.random() > 0.5:
+                    c.genome[i] = (random.randint(gene_range[0],gene_range[1]),c.genome[i][1] + random.randint(0,mutate_max))
+                else:
+                    c.genome[i] = (random.randint(gene_range[0],gene_range[1]),c.genome[i][1] - random.randint(0,mutate_max))
+        return c
+    
+    def crossover(self,organism):
+        ''' Return an organism that is a crossover between this organism and the provided organism '''
+        c = self.copy()
+        for i in range(len(c.genome)):
+            if random.random() > 0.5:
+                c.genome[i] = organism.genome[i]
+        return c
+    
+    def onepointcrossover(self,organism):
+        inflection_point = random.randint(0,len(organism.genome)-1)
+        
+        c1 = self.copy()
+        c2 = organism.copy()
+        
+        genome1 = c1.genome[:inflection_point] + c2.genome[inflection_point:]
+        genome2 = c2.genome[:inflection_point] + c1.genome[inflection_point:]
+        
+        c1.genome = genome1
+        c2.genome = genome2
+        
+        return [c1,c2]
+    
+    def calculate_fitness(self,target):
+        ''' Calculate the fitness of this organism '''
+        # First, must resolve
+        result_path = self.resolve(target[0],target[1],record_path=True)
+        final_pattern = target[1]
+        self.fitness = 1
+        for level,result in enumerate(result_path):
+            p_common = 0
+            p_correct = 0
+            p_common = float(len([1 for item in result if item in final_pattern]))/float(max(len(result),len(final_pattern)))
+            for idx,item in enumerate(result):
+                if idx < len(final_pattern) and item == final_pattern[idx]:
+                    p_correct += 1
+            p_correct = float(p_correct)/float(max(len(result),len(final_pattern)))
+            fitness = 1.0 - 0.5*(p_common + p_correct)
+            if fitness < self.fitness:
+                self.fitness = fitness
+                self.fitness_level = level
+    
+    def copy(self):
+        c = OperationOrganism()
+        c.genome = self.genome[:]
+        return c
+        
+    def resolve(self,start_pattern,final_pattern,record_path=False):
+        result = start_pattern[:]
+        if record_path:
+            path = [result[:]]
+        for operation_tuple in self.genome:
+            operation = operation_tuple[0]
+            operand = operation_tuple[1]
+            if operation == 1:
+                # no op
+                pass
+            elif operation == 2:
+                # add
+                #index = random.randint(0,len(final_pattern)-1)
+                index = operand % len(final_pattern)
+                result[index:index] = [final_pattern[index]]
+            elif operation == 3 and len(result) > 0:
+                # delete
+                #index = random.randint(0,len(result)-1)
+                index = operand % len(result)
+                del result[index]
+            elif operation == 4 and len(result) > 0:
+                # mutate
+                #index = random.randint(0,min(len(result)-1,len(final_pattern)-1))
+                index = operand % min(len(result),len(final_pattern))
+                result[index] = final_pattern[index]
+            elif operation == 5 and len(result) > 0 and operand != 0:
+                # rotation
+                amount = (operand/abs(operand)) * (operand % len(result))
+                result = shift(result,amount)
+            elif operation == 6:
+                # exchange
+                pass
+            elif operation == 7:
+                # incorrect rotation
+                pass
+            elif operation == 8:
+                # incorrect exchange
+                pass
+            if record_path:
+                path.append(result[:])
+        if record_path:
+            return path
+        else:
+            return result
+    
+    def best_path(self,start_pattern,final_pattern,condense=1):
+        to_return = []
+        result_path = self.resolve(start_pattern,final_pattern,record_path=True)
+        for i in range(0,self.fitness_level,condense):
+            to_return += result_path[i]
+        to_return += final_pattern
+        return to_return
---
+++ thematic_bridge_example.py
@@ -0,0 +1,43 @@
+from Organism import Organism
+from Population import Population
+from WAVWriter import WAVWriter
+
+tomorrow_combined = [(52,4),(54,8),(52,4),(49,8),(54,8),(52,4),(49,8),(54,8),(52,4),(45,8),(52,8),(50,4),(47,8),(50,8),(49,4),(45,8),(40,4),(47,4),(45,1)]
+tomorrow = [52,54,52,49,54,52,49,54,52,45,52,50,47,50,49,45,40,47,45]
+tomorrow_time = [4,8,4,8,8,4,8,8,4,8,8,4,8,8,4,8,4,4,1]
+yesterday_combined = [(47, 8), (45, 8), (45, -2), (0, 4), (49, 8), (51, 8), (53, 8), (54, 8), (56, 8), (57, 8), (56, -8), (54, 16), (54, -2), (0, 4), (54, 8), (54, 8), (52, 8), (50, 8), (49, 8), (47, 8), (50, 4), (49, 8), (49, -4)]
+yesterday   = [47,45,45,0,49,51,53,54,56,57,56,54,54,0,54,54,52,50,49,47,50,49,49]
+yesterday_time = [8,8,-2,4,8,8,8,8,8,8,-8,16,-2,4,8,8,8,8,8,8,4,8,-4]
+
+#sun = [(0, 4), (53, 8), (51, 4), (53, 4), (50, -4), (53, 8), (50, 8), (51, 8), (53, -4), (0, 4), (53, 8), (51, 4), (53, 4), (50, -4), (50, 8), (51, 4), (50, 4), (0, 8), (53, 4), (51, 4), (50, 4), (44, 8), (46, 8), (50, 8), (51, 8), (44, 8), (50, 8), (51, 8), (42, 8), (50, 8), (51, 8), (44, 8), (50, 8), (51, 8), (50, 8), (48, 8), (46, 8), (44, 8)]
+sun = [(0, 4), (52, 8), (50, 4), (52, 4), (48, -4), (52, 8), (48, 8), (50, 8), (52, -4), (0, 4), (52, 8), (50, 4), (52, 4), (48, -4), (48, 8), (50, 4), (48, 4), (0, 8), (52, 4), (50, 4), (48, 4), (43, 8), (45, 8), (48, 8), (50, 8), (43, 8), (48, 8), (50, 8), (41, 8), (48, 8), (50, 8), (43, 8), (48, 8), (50, 8), (48, 8), (47, 8), (45, 8), (43, 8)]
+
+p_melody = Population(100,None,(yesterday_combined,sun),(1,5),1)
+
+w = WAVWriter()
+#w.write(p_melody.best(1)[0].resolve(yesterday,tomorrow),"output/"+str(p_melody.number_of_generations)+".wav")
+#w.write(tomorrow,"output/final.wav")
+print p_melody.best(1)[0].resolve(yesterday_combined,sun),"gens:", p_melody.number_of_generations, "melody:",p_melody.most_fit()
+print tomorrow
+last_fit = 0.0
+last_gen = 0
+try:
+    while p_melody.most_fit() > 0.1:
+        print p_melody.most_fit()
+        p_melody.generation()
+        if not last_fit or last_fit - p_melody.most_fit() > 0.05 or p_melody.number_of_generations-last_gen > 100:
+            last_fit = p_melody.most_fit()
+            last_gen = p_melody.number_of_generations
+            print p_melody.best(1)[0].resolve(yesterday_combined,sun),"gens:", p_melody.number_of_generations, "melody:",p_melody.most_fit(),"level:",p_melody.best(1)[0].fitness_level
+            print tomorrow
+            print p_melody.summary(5)
+            #w.write(p_melody.best(1)[0].resolve(yesterday,tomorrow),"output/"+str(p_melody.number_of_generations)+".wav")
+        elif p_melody.number_of_generations > 10000:
+            break
+except KeyboardInterrupt:
+    print "STOPPED"
+print "OUT"
+print p_melody.best(1)[0].resolve(yesterday_combined,sun),"gens:", p_melody.number_of_generations, "melody:",p_melody.most_fit(),"level:",p_melody.best(1)[0].fitness_level
+print tomorrow
+w.write(p_melody.best(1)[0].resolve(yesterday_combined,sun),"output/"+str(p_melody.number_of_generations)+"_final.wav")
+w.write(p_melody.best(1)[0].best_path(yesterday_combined,sun,4),"output/"+str(p_melody.number_of_generations)+"_path.wav")
